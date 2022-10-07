c755bba57f976466ca19885af37cdb4b7ab934fa
Daniele Trovato <plasticsoup@yahoo.co.uk>
2022-10-07 21:01:16 +0100

    Add complete retake of app.js

--- app.js
+++ app.js
@@ -1,84 +1,76 @@
-function Game(player1, player2) {
-  this.player1 = { name: player1, score: 0, win: false };
-  this.player2 = { name: player2, score: 0, win: false };
-  this.gameOn = true;
+function Game(server, receiver) {
+  this.scores = { [server]: 0, [receiver]: 0 };
 
-  // make private function to return array with passed player as index 0 and second player at index 1
-  const playersArray = (player) => {
-    const pl1 = this.player1;
-    const pl2 = this.player2;
+  const pointNames = { 0: "0", 1: "15", 2: "30", 3: "40" };
 
-    // if player is incorrect return error
-    if (![pl1.name, pl2.name].includes(player)) {
-      return "Player is off today";
+  function scoreDifference(serverScore, receiverScore) {
+    return Math.abs(serverScore - receiverScore);
+  }
+
+  function leadPlayer() {
+    return score(server) > score(receiver) ? server : receiver;
+  }
+
+  // if both scores are 3 or above and they are the same return true
+  function isDeauce() {
+    if (
+      score(server) >= 3 &&
+      score(receiver) >= 3 &&
+      score(server) === score(receiver)
+    ) {
+      return true;
     }
 
-    return player === pl1.name ? [pl1, pl2] : [pl2, pl1];
-  };
+    return false;
+  }
 
-  // private function
-  const declareWinner = (player1, player2) => {
-    if (player1.win) return `game ${player1.name}`;
-    if (player2.win) return `game ${player2.name}`;
-  };
-
-  // private function
-  const declareAdvantage = (player1, player2) => {
-    if (player1.score === "advantage") return `advantage ${player1.name}`;
-    if (player2.score === "advantage") return `advantage ${player2.name}`;
-  };
-
-  // private function
-  const detectDeauce = (player1, player2) => {
+  // if the difference between scores is one point and whey are both 3 or above return true
+  function isAdvantage() {
     if (
-      (player1.score === 40 && player2.score === 40) ||
-      (player1.score === "deauce" && player2.score === "deauce")
+      scoreDifference(score(server), score(receiver)) === 1 &&
+      score(server) >= 3 &&
+      score(receiver) >= 3
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  // get the score of the player passed in
+  const score = (playerName) => {
+    return this.scores[playerName]; // ex: 2, this is an integer
+  };
+
+  // if the difference between scores is more than one point and at least one of them is greater than 3 return true
+  function isGameOver() {
+    if (
+      scoreDifference(score(server), score(receiver)) > 1 &&
+      (score(server) > 3 || score(receiver) > 3)
     )
       return true;
-  };
+  }
 
-  this.pointTo = function (player) {
-    // the argument 'player' will always be playerA
-    const [playerA, playerB] = playersArray(player);
-    const scoreA = playerA.score;
-    const scoreB = playerB.score;
-    // the points will only be added while the game is on. it stops being on when a winner is declared
-    if (this.gameOn) {
-      if (scoreA < 30) {
-        playerA.score += 15;
-      } else if (scoreA === 30) {
-        playerA.score += 10;
-      } else if (scoreA === 40) {
-        if (scoreB < 40) {
-          playerA.win = true;
-          this.gameOn = false;
-        } else if (scoreB === 40) {
-          playerA.score = "advantage";
-        }
-      } else if (scoreA === "advantage" && scoreB === "deauce") {
-        playerA.win = true;
-        this.gameOn = false;
-      } else if (scoreA === "deauce") {
-        playerA.score = "advantage";
-      }
-    }
+  // increment the score referenced by playerName in the this.scores object
+  this.pointTo = function (playerName) {
+    this.scores[playerName] += 1;
   };
 
   this.score = function () {
-    const player1 = this.player1;
-    const player2 = this.player2;
+    if (isDeauce()) return "deauce";
 
-    if (player1.win || player2.win) return declareWinner(player1, player2);
+    if (isAdvantage()) return `advantage ${leadPlayer()}`;
 
-    if (player1.score === "advantage" || player2.score === "advantage")
-      return declareAdvantage(player1, player2);
-
-    if (detectDeauce(player1, player2)) {
-      return "players are deauce";
+    if (isGameOver()) {
+      return `game ${leadPlayer()}`;
     }
 
-    return `${player1.name}'s score is ${player1.score}. ${player2.name}'s score is ${player2.score}`;
+    // return score in the form of ex. d: 40, c: 30 if the above conditions don't run
+    return `${server}: ${pointNames[this.scores[server]]}, ${receiver}: ${
+      pointNames[this.scores[receiver]]
+    }`;
   };
 }
 
 module.exports = { Game };
+
+// make an array of scores and sort it
--- test/test.js
+++ test/test.js
@@ -4,95 +4,20 @@ const Game = app.Game;
 
 describe("Game", function () {
   describe("#score()", function () {
-    it("should return 15 0", function () {
+    it("should return d: 15, c: 0", function () {
       const game = new Game("d", "c");
       game.pointTo("d");
-      assert.equal(game.score(), "d's score is 15. c's score is 0");
+      assert.equal(game.score(), "d: 15, c: 0");
     });
 
-    it("should return 30 0", function () {
-      const game = new Game("d", "c");
-      game.pointTo("d");
-      game.pointTo("d");
-      assert.equal(game.score(), "d's score is 30. c's score is 0");
-    });
-
-    it("should return 40 0", function () {
+    it("should return d: 40, c: 30", function () {
       const game = new Game("d", "c");
       game.pointTo("d");
       game.pointTo("d");
       game.pointTo("d");
-      assert.equal(game.score(), "d's score is 40. c's score is 0");
-    });
-
-    it("should return 40 15", function () {
-      const game = new Game("d", "c");
-      game.pointTo("d");
-      game.pointTo("d");
-      game.pointTo("c");
-      game.pointTo("d");
-      assert.equal(game.score(), "d's score is 40. c's score is 15");
-    });
-
-    it("should return 40 30", function () {
-      const game = new Game("d", "c");
-      game.pointTo("d");
-      game.pointTo("d");
-      game.pointTo("c");
-      game.pointTo("d");
-      game.pointTo("c");
-      assert.equal(game.score(), "d's score is 40. c's score is 30");
-    });
-
-    it("should return deauce", function () {
-      const game = new Game("d", "c");
-      game.pointTo("d");
-      game.pointTo("d");
-      game.pointTo("c");
-      game.pointTo("d");
       game.pointTo("c");
       game.pointTo("c");
-      assert.equal(game.score(), "players are deauce");
-    });
-
-    it("should return 0 15", function () {
-      const game = new Game("d", "c");
-      game.pointTo("c");
-      assert.equal(game.score(), "d's score is 0. c's score is 15");
-    });
-
-    it("should return 0 30", function () {
-      const game = new Game("d", "c");
-      game.pointTo("c");
-      game.pointTo("c");
-      assert.equal(game.score(), "d's score is 0. c's score is 30");
-    });
-
-    it("should return 0 40", function () {
-      const game = new Game("d", "c");
-      game.pointTo("c");
-      game.pointTo("c");
-      game.pointTo("c");
-      assert.equal(game.score(), "d's score is 0. c's score is 40");
-    });
-
-    it("should return 15 40", function () {
-      const game = new Game("d", "c");
-      game.pointTo("c");
-      game.pointTo("c");
-      game.pointTo("c");
-      game.pointTo("d");
-      assert.equal(game.score(), "d's score is 15. c's score is 40");
-    });
-
-    it("should return 30 40", function () {
-      const game = new Game("d", "c");
-      game.pointTo("d");
-      game.pointTo("c");
-      game.pointTo("c");
-      game.pointTo("c");
-      game.pointTo("d");
-      assert.equal(game.score(), "d's score is 30. c's score is 40");
+      assert.equal(game.score(), "d: 40, c: 30");
     });
 
     it("should return deauce", function () {
@@ -103,91 +28,55 @@ describe("Game", function () {
       game.pointTo("c");
       game.pointTo("c");
       game.pointTo("c");
-      assert.equal(game.score(), "players are deauce");
+      game.pointTo("c");
+      game.pointTo("d");
+      assert.equal(game.score(), "deauce");
+    });
+
+    it("should return advantage c", function () {
+      const game = new Game("d", "c");
+      game.pointTo("d");
+      game.pointTo("d");
+      game.pointTo("d");
+      game.pointTo("c");
+      game.pointTo("c");
+      game.pointTo("c");
+      game.pointTo("c");
+      assert.equal(game.score(), "advantage c");
     });
 
     it("should return advantage d", function () {
       const game = new Game("d", "c");
       game.pointTo("d");
-      game.pointTo("d");
-      game.pointTo("d");
       game.pointTo("c");
       game.pointTo("c");
+      game.pointTo("d");
+      game.pointTo("d");
       game.pointTo("c");
       game.pointTo("d");
       assert.equal(game.score(), "advantage d");
     });
 
-    it("should return advantage c", function () {
-      const game = new Game("d", "c");
-      game.pointTo("c");
-      game.pointTo("d");
-      game.pointTo("c");
-      game.pointTo("d");
-      game.pointTo("c");
-      game.pointTo("d");
-      game.pointTo("c");
-      assert.equal(game.score(), "advantage c");
-    });
-
     it("should return game d", function () {
       const game = new Game("d", "c");
       game.pointTo("d");
       game.pointTo("d");
-      game.pointTo("c");
       game.pointTo("d");
       game.pointTo("c");
       game.pointTo("d");
       assert.equal(game.score(), "game d");
     });
 
-    it("should return game d", function () {
-      const game = new Game("d", "c");
-      game.pointTo("d");
-      game.pointTo("d");
-      game.pointTo("c");
-      game.pointTo("d");
-      game.pointTo("c");
-      game.pointTo("d");
-      game.pointTo("c");
-      game.pointTo("c");
-      game.pointTo("c");
-      game.pointTo("c");
-      game.pointTo("c");
-      game.pointTo("c");
-      game.pointTo("c");
-      game.pointTo("c");
-      game.pointTo("c");
-      assert.equal(game.score(), "game d");
-    });
-
     it("should return game c", function () {
       const game = new Game("d", "c");
-      game.pointTo("c");
       game.pointTo("d");
-      game.pointTo("c");
       game.pointTo("d");
-      game.pointTo("c");
-      game.pointTo("c");
-      assert.equal(game.score(), "game c");
-    });
-
-    it("should return game c", function () {
-      const game = new Game("d", "c");
-      game.pointTo("c");
-      game.pointTo("d");
-      game.pointTo("c");
       game.pointTo("d");
       game.pointTo("c");
       game.pointTo("c");
-      game.pointTo("d");
-      game.pointTo("d");
-      game.pointTo("d");
-      game.pointTo("d");
-      game.pointTo("d");
-      game.pointTo("d");
-      game.pointTo("d");
-      game.pointTo("d");
+      game.pointTo("c");
+      game.pointTo("c");
+      game.pointTo("c");
       assert.equal(game.score(), "game c");
     });
   });
